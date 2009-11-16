2e94ad9932ac79770c07c931ace2952b09cc19ad
Hendrik Kueck <kueck@cs.ubc.ca>
2009-11-15 23:30:37 -0800

    added explanatory comments to app delegate

--- Classes/PhotoAppLinkTestAppAppDelegate.m
+++ Classes/PhotoAppLinkTestAppAppDelegate.m
@@ -16,10 +16,27 @@
 @synthesize window;
 @synthesize navigationController;
 
+// The following is the custom URL scheme that will be used by other apps to invoke this app
+// Recommended format is "nameofyourapp-photoapplink"
+// You have to define this URL scheme in your app's info.plist (see: http://bit.ly/YS49o)
+// Your app and its URL scheme also need to be added to a plist stored on a server to be discoverable
+// by other apps implementing the Photo App Link protocol.
+static NSString* const APP_PHOTOAPPLINK_URL_SCHEME = @"photoapplinktestapp-photoapplink";
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions 
 {
+    // NOTE: If your app already uses the handleOpenURL: scheme, it is probably best to stick with that.
+    //       Implementing didFinishLaunchingWithOptions: when running under 3.0 causes this method to 
+    //       always be called rather than the applicationDidFinishLaunching: & handleOpenURL: combination.
+    //       Under 2.x didFinishLaunchingWithOptions: will be ignored, so a different code path will be taken at launch.
+    //       If your code does not currently implement handleOpenURL: I would recommend the scheme used in this example: 
+    //          use didFinishLaunchingWithOptions: to handle URL & notification launches, forward normal launches 
+    //          to applicationDidFinishLaunching: (which will also be called directly when running under 2.x)
+    
     // Simply accessing the applink manager here triggers a background update of the list of supported apps
     // (if necessary). So do this first thing. 
+    // If you are using applicationDidFinishLaunching: & handleOpenURL: make sure you stick this into 
+    // applicationDidFinishLaunching: in order to trigger the implicit background update.
     PhotoAppLinkManager* applink = [PhotoAppLinkManager sharedPhotoAppLinkManager];
 
     // If you are not sure about your app's bundle ID, this prints it:
@@ -28,14 +45,16 @@
     
     if (launchOptions) {
         NSURL* launchURL = [launchOptions objectForKey:@"UIApplicationLaunchOptionsURLKey"];
-        if ([[launchURL scheme] isEqualToString:@"photoapplinktestapp-photoapplink"]) {
-            // launched from another app
+        if ([[launchURL scheme] isEqualToString:APP_PHOTOAPPLINK_URL_SCHEME]) {
+            // launched from another app via our Photo App Link URL scheme
             
             // basic app setup
             [window addSubview:navigationController.view];
             [window makeKeyAndVisible];
                         
-            // get the image that was passed from previous app
+            // Retrieve the image that was passed from previous app.
+            // You could (and maybe should) instead access this image during a later stage of the launch process 
+            // rather than doing it directly in this launch handler
             UIImage *image = [applink popPassedInImage];
             [rootViewController performSelector:@selector(setImage:) withObject:image afterDelay:0.0];
             
