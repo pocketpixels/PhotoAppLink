988b36d6b0065d4a6e02aaf967505137bae9e1ec
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2011-05-26 23:25:17 -0300

    Autogenerated files for the send to view controller

---
+++ PhotoAppLink/PhotoAppLinkSendToController.h
@@ -0,0 +1,16 @@
+//
+//  PhotoAppLinkSendToController.h
+//  PhotoAppLinkTestApp
+//
+//  Created by Gustavo Ambrozio on 26/5/11.
+//  Copyright 2011 CodeCrop Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PhotoAppLinkSendToController : UIViewController {
+    
+}
+
+@end
---
+++ PhotoAppLink/PhotoAppLinkSendToController.m
@@ -0,0 +1,57 @@
+//
+//  PhotoAppLinkSendToController.m
+//  PhotoAppLinkTestApp
+//
+//  Created by Gustavo Ambrozio on 26/5/11.
+//  Copyright 2011 CodeCrop Software. All rights reserved.
+//
+
+#import "PhotoAppLinkSendToController.h"
+
+
+@implementation PhotoAppLinkSendToController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
