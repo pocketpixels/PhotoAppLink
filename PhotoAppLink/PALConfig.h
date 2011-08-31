f4e5d993e838819889b56f4531cb8dd5c79c6a6b
steve <steve@eastcoastpixels.com>
2011-08-31 00:35:52 -0400

    allow config by delegate

--- PhotoAppLink/PALConfig.h
+++ PhotoAppLink/PALConfig.h
@@ -2,6 +2,7 @@
 //  PALConfig.h
 //
 
+// You can edit these constants directly, or provide a delegate that provides the values
 
 // Substitute this for testing with your own edited server side plist URL
 // (Make sure to set up your XCode project so that DEBUG is defined in debug builds, 
@@ -12,7 +13,7 @@
 // If you are not using app icons to display the list of supported apps in your UI 
 // (for example because you only use the action sheet displaying the app names)
 // you can set this to NO to disable downloading of these app icons
-#define USING_APP_ICONS     YES
+#define USING_APP_ICONS     @"YES"	// see NSString's boolValue, no is @"0"
 
 
 // Substitute your own Linkshare Site ID here if you like.
@@ -24,3 +25,33 @@
 // The image to use for apps with missing icons
 // The file has to be included in the app bundle
 #define GENERIC_APP_ICON    @"PAL_unknown_app_icon.png"
+
+
+
+@protocol PALConfigDelegate;
+
+@interface PALConfig : NSObject {
+	id <PALConfigDelegate> delegate;
+}
+
+@property (nonatomic,readonly, retain) id <PALConfigDelegate> delegate;
+
++ (PALConfig*)sharedInstance;
++ (PALConfig*)sharedInstanceWithDelegate:(id <PALConfigDelegate>)delegate;
+
+- (id)initWithDelegate:(id <PALConfigDelegate>)delegate;
+- (id)configurationValue:(NSString*)selector;
+
+#define PALCONFIG(_CONFIG_KEY) [[PALConfig sharedInstance] configurationValue:@#_CONFIG_KEY]
+
+@end
+
+
+@protocol PALConfigDelegate <NSObject>
+@optional
+
+- (NSString*)debugPListURL;
+- (NSString*)usingAppIcons;
+- (NSString*)linkShareSiteID;
+- (NSString*)genericAppIcon;
+@end
---
+++ PhotoAppLink/PALConfig.m
@@ -0,0 +1,148 @@
+//
+//  PALConfig.m
+//
+
+#import "PALConfig.h"
+
+static PALConfig *sharedInstance = nil;
+
+////////LegacyPALConfigDelegate
+@interface LegacyPALConfigDelegate : NSObject <PALConfigDelegate>  {
+	NSDictionary *configuration;
+}
+@end
+
+@implementation LegacyPALConfigDelegate
+- (id)init
+{
+    if ((self = [super init])) {
+		configuration = [[NSDictionary alloc] initWithObjectsAndKeys:
+						 DEBUG_PLIST_URL, @"debugPListURL", 
+						 USING_APP_ICONS, @"usingAppIcons", 
+						 LINKSHARE_SITE_ID, @"linkShareSiteID", 
+						 GENERIC_APP_ICON, @"genericAppIcon", 
+						 nil];
+	}
+	
+	//NSLog(@"Legacy configuration: %@", configuration);
+	
+    return self;	
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector
+{
+	return [super respondsToSelector:aSelector] || [configuration objectForKey:NSStringFromSelector(aSelector)] != nil;
+}
+
+- (id) performSelector:(SEL)aSelector
+{
+	id configValue = [configuration objectForKey:NSStringFromSelector(aSelector)];
+	if(configValue == nil) {
+		return [super performSelector:aSelector];
+	} else {
+		return configValue;
+	}
+}
+@end
+////////LegacyPALConfigDelegate
+
+
+////////PALConfig
+@interface PALConfig()
+@property (nonatomic, retain) id <PALConfigDelegate> delegate;
+@end
+
+
+@implementation PALConfig
+
+@synthesize delegate;
+
+#pragma mark -
+#pragma mark Instance methods
+
+- (id)configurationValue:(NSString*)selector
+{
+	NSLog(@"Looking for a configuration value for %@.", selector);
+	
+	SEL sel = NSSelectorFromString(selector);
+	if ([delegate respondsToSelector:sel]) {
+		id value = [delegate performSelector:sel];
+		if (value) {
+			NSLog(@"Found configuration value for %@: %@", selector, value);
+			return value;
+		}
+	}
+	
+	NSLog(@"Didn't find a configuration value for %@.", selector);
+	return nil;
+}
+
+#pragma mark -
+#pragma mark Singleton methods
+
+// Singleton template based on http://stackoverflow.com/questions/145154
+
++ (PALConfig*)sharedInstance
+{
+    @synchronized(self)
+    {
+        if (sharedInstance == nil)
+			sharedInstance = [[PALConfig alloc] initWithDelegate:[[LegacyPALConfigDelegate alloc] init]];
+    }
+    return sharedInstance;
+}
+
++ (PALConfig*)sharedInstanceWithDelegate:(id <PALConfigDelegate>)delegate
+{
+    @synchronized(self)
+    {
+		if (sharedInstance != nil) {
+			[NSException raise:@"IllegalStateException" format:@"PALConfig has already been configured with a delegate."];
+		}
+		sharedInstance = [[PALConfig alloc] initWithDelegate:delegate];
+    }
+    return sharedInstance;
+}
+
+
++ (id)allocWithZone:(NSZone *)zone {
+    @synchronized(self) {
+        if (sharedInstance == nil) {
+            sharedInstance = [super allocWithZone:zone];
+            return sharedInstance;  // assignment and return on first allocation
+        }
+    }
+    return nil; // on subsequent allocation attempts return nil
+}
+
+- (id)initWithDelegate:(id <PALConfigDelegate>)delegateIn
+{
+    if ((self = [super init])) {
+		self.delegate = delegateIn;
+    }
+    return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return self;
+}
+
+- (id)retain {
+    return self;
+}
+
+- (unsigned)retainCount {
+    return UINT_MAX;  // denotes an object that cannot be released
+}
+
+- (void)release {
+    //do nothing
+}
+
+- (id)autorelease {
+    return self;
+}
+////////PALConfig
+
+@end
